4a9f9bddaf83f827242ffb4a251f47c827869043
sycinue-rdc <geraldeulicys.rdc@gmail.com>
2021-12-20 03:20:14 +0800

    main site

---
+++ site/assets/js/htmlscript.js
@@ -0,0 +1,13 @@
+anchors.add();
+
+AOS.init()
+particlesJS.load('particles', '../json/particles.json', function () {
+var canvas = document.querySelector(".particles-js-canvas-el")
+});
+scrolling = document.querySelectorAll('.acad-description')
+scrolling.forEach((x, i) => {
+var m = new PerfectScrollbar(x, {
+    wheelSpeed: 0.2,
+    wheelPropagation: true,
+});
+})
